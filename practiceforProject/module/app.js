69c2182821fda6d46f5d6c4f2c61ec514007fb4e
HongMoonGI <hmoongi94@gmail.com>
2023-10-24 16:40:32 +0900

    preventDefault사용 이유 설명에대한 주석처리

--- practiceforProject/module/app.js
+++ practiceforProject/module/app.js
@@ -2,7 +2,9 @@
 const menuItems = document.querySelectorAll("ul#menu li a");
 menuItems.forEach(item => {
     item.addEventListener("click", (event) => {
+        console.dir(event)
         event.preventDefault();
+        // a태그에는 링크로 연결하는 동작이 default값으로 설정되어있는데, preventDefault를 사용하면 이 동작을 막고 아래에 작성한 동작을 실행함.
         const targetId = event.target.getAttribute("href").substring(1); // # 제거
         const targetSection = document.getElementById(targetId);
         if (targetSection) {
