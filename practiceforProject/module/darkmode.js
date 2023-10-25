b810da9a1781eb2c47c3cc26b555eebc2b01f484
HongMoonGI <hmoongi94@gmail.com>
2023-10-25 17:29:27 +0900

    다시 처음부터 수정(다크모드)

--- practiceforProject/module/darkmode.js
+++ practiceforProject/module/darkmode.js
@@ -1,7 +1,7 @@
 // dark-mode.js
 const darkModeToggle = document.getElementById('dark-mode-toggle');
 const body = document.body;
-const dayModeIcon = document.getElementById('day-mode-icon');
+const darkModeIcon = document.getElementById('dark-mode-icon');
 
 darkModeToggle.addEventListener('click', () => {
     const currentMode = darkModeToggle.getAttribute('data-mode');
@@ -9,14 +9,12 @@ darkModeToggle.addEventListener('click', () => {
     // 모드 토글
     if (currentMode === 'day') {
         darkModeToggle.setAttribute('data-mode', 'dark');
-        body.classList.remove('day-mode');
-        dayModeIcon.src = './img/dark-icon.png';
-        dayModeIcon.style.marginLeft = "50px";
+        body.classList.add('dark-mode');
+        darkModeIcon.src = 'dark-icon.png';
     } else {
         darkModeToggle.setAttribute('data-mode', 'day');
-        body.classList.add('day-mode');
-        dayModeIcon.src = './img/day-icon.png';
-        dayModeIcon.style.marginLeft= "0px";
+        body.classList.remove('dark-mode');
+        darkModeIcon.src = 'day-icon.png';
     }
 });
 
