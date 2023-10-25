7331b69f5e75f317fd3ad8fa2f21aa214e999623
HongMoonGI <hmoongi94@gmail.com>
2023-10-25 15:55:51 +0900

    사진이 변경이 안됨

---
+++ practiceforProject/module/darkmode.js
@@ -0,0 +1,26 @@
+// dark-mode.js
+const darkModeToggle = document.getElementById('dark-mode-toggle');
+const body = document.body;
+const darkModeIcon = document.getElementById('dark-mode-icon');
+
+darkModeToggle.addEventListener('click', () => {
+    const currentMode = darkModeToggle.getAttribute('data-mode');
+
+    // 모드 토글
+    if (currentMode === 'day') {
+        darkModeToggle.setAttribute('data-mode', 'dark');
+        body.classList.remove('day-mode');
+        darkModeIcon.src = './img/dark-icon.png';
+    } else {
+        darkModeToggle.setAttribute('data-mode', 'day');
+        body.classList.add('day-mode');
+        darkModeIcon.src = './img/day-icon.png';
+    }
+});
+
+// 사용자의 환경 설정에 따라 초기 모드 설정
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    darkModeToggle.setAttribute('data-mode', 'dark');
+    body.classList.add('dark-mode');
+    darkModeIcon.src = 'dark-icon.png';
+}
