c440aa010124e851a2ae06c0dba7addd0cf537fe
HongMoonGI <hmoongi94@gmail.com>
2023-11-23 14:06:08 +0900

    feat: 로그인 폼에 addeventlistener달기

--- project-exerciseInformationCommunity/test_login/login.js
+++ project-exerciseInformationCommunity/test_login/login.js
@@ -1,28 +1,26 @@
 export const load = function () {
   document.addEventListener('DOMContentLoaded', function () {
-    const loginSubmit = document.getElementById('loginsubmit')
-    const signupSubmit = document.getElementById('signupsubmit')
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    const signupEmail = document.getElementById('signupEmail').value;
-    const signupPassword = document.getElementById('signupPassword').value;
-
+    const loginSubmit = document.getElementById('loginSubmit')
+    const signupSubmit = document.getElementById('signupSubmit')
+    
     // 여기서 AJAX 또는 Fetch API를 사용하여 서버로 데이터 전송
     // 서버에서 인증을 수행하고 적절한 응답을 처리
     loginSubmit.addEventListener("click", function () {
-      console.log(`Login: Email - ${email}, Password - ${password}`);
-
+      const email = document.getElementById('email').value
+      const password = document.getElementById('password').value
+      console.log(`Login: Email - ${email}, Password - ${password}`)
     })
-
-
-
+    
+    
+    
     // 여기서 AJAX 또는 Fetch API를 사용하여 서버로 데이터 전송
     // 서버에서 회원가입 처리하고 적절한 응답을 처리
     signupSubmit.addEventListener("click", function () {
-      console.log(`Sign Up: Email - ${signupEmail}, Password - ${signupPassword}`);
-
+      const signupEmail = document.getElementById('signupEmail').value
+      const signupPassword = document.getElementById('signupPassword').value
+      console.log(`Sign Up: Email - ${signupEmail}, Password - ${signupPassword}`)
     })
-
+    
     
   })
 }
