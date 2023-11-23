2f62682bb6bad01177ff43e1df0cdad869d6d74a
HongMoonGI <hmoongi94@gmail.com>
2023-11-23 16:01:50 +0900

    mongoose database 사용해보기 Fixes #5

--- project-exerciseInformationCommunity/app.js
+++ project-exerciseInformationCommunity/app.js
@@ -1,17 +1,29 @@
+//* server module
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const path = require('path');
-const { loginRequest,signupRequest } = require('./public/route/router.js');
+
+const { loginRequest, signupRequest } = require('./public/route/router.js');
 
 const app = express();
 const port = 3000;
 
+//* 미들웨어 설정
 app.use(express.static(path.join(__dirname, './public/static')))
 app.use(bodyParser.json());
 
-app.post('/login', loginRequest)
-app.post('/signup',signupRequest)
+//* MongoDB 연결 설정
+mongoose.connect('mongodb://localhost:27017/mydatabase', { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
 
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+//* 라우팅
+app.post('/login', loginRequest)
+app.post('/signup', signupRequest)
+
+//* 서버가동
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
