dc14e3962007b77c572350fd27ce7cb48a6b277c
HongMoonGI <hmoongi94@gmail.com>
2024-01-26 17:24:22 +0900

    correction

--- project-exerciseApp/memo.ts
+++ project-exerciseApp/memo.ts
@@ -1,4 +1,5 @@
-//* export default를 사용할 때:
-// import example from ~~
-//* export를 사용할 때:
-// import {example} from ~~
+//? export default와 export의 차이
+  //* export default를 사용할 때:
+    // import example from ~~
+  //* export를 사용할 때:
+    // import {example} from ~~
