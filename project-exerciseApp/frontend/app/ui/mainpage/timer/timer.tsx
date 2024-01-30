0b793867b8107e814342607ac6b7b182a0e75891
HongMoonGI <hmoongi94@gmail.com>
2024-01-30 11:41:28 +0900

    fix: 타이머 기능 구현

--- project-exerciseApp/frontend/app/ui/mainpage/timer/timer.tsx
+++ project-exerciseApp/frontend/app/ui/mainpage/timer/timer.tsx
@@ -2,28 +2,22 @@ import React, { useState, useEffect } from 'react';
 
 const Timer = () => {
   const [seconds, setSeconds] = useState(0);
-  const [intervalId, setIntervalId] = useState<NodeJS.Timeout | undefined>(undefined);
+  const [intervalId, setIntervalId] = useState<number | undefined>(undefined);
   const [isActive, setIsActive] = useState(false);
 
   useEffect(() => {
+    let intervalId: NodeJS.Timeout;
+
     if (isActive) {
-      const id: NodeJS.Timeout = setInterval(() => {
+      intervalId = setInterval(() => {
         setSeconds((prevSeconds) => prevSeconds + 1);
       }, 1000);
-
-      setIntervalId(id);
-    } else {
-      if (intervalId !== undefined) {
-        clearInterval(intervalId);
-      }
     }
 
     return () => {
-      if (intervalId !== undefined) {
-        clearInterval(intervalId);
-      }
+      clearInterval(intervalId);
     };
-  }, [isActive, intervalId]);
+  }, [isActive]);
 
   const handleStartStop = () => {
     setIsActive((prevIsActive) => !prevIsActive);
