5c594e9868e173400e509612032236fbb502941e
HongMoonGI <hmoongi94@gmail.com>
2024-01-30 10:43:16 +0900

    feat: 메인페이지 기능 추가

---
+++ project-exerciseApp/frontend/app/ui/mainpage/calender/calender.tsx
@@ -0,0 +1,11 @@
+
+const Calendar = () => {
+  return (
+    <div>
+      <h1>Calendar</h1>
+      {/* 캘린더 컴포넌트 내용 추가 */}
+    </div>
+  );
+};
+
+export default Calendar;
---
+++ project-exerciseApp/frontend/app/ui/mainpage/timer/timer.tsx
@@ -0,0 +1,10 @@
+const Timer = () => {
+  return (
+    <div>
+      <h1>Timer</h1>
+      {/* 타이머 컴포넌트 내용 추가 */}
+    </div>
+  );
+};
+
+export default Timer;
