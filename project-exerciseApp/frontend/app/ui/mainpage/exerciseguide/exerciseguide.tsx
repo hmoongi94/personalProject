bdf918223334457b4aa5fa2d831cd12be6c776dc
HongMoonGI <hmoongi94@gmail.com>
2024-01-31 13:05:41 +0900

    feat: css

--- project-exerciseApp/frontend/app/ui/mainpage/exerciseguide/exerciseguide.tsx
+++ project-exerciseApp/frontend/app/ui/mainpage/exerciseguide/exerciseguide.tsx
@@ -15,18 +15,19 @@ interface ExerciseGuideProps {
 
 const ExerciseGuide: React.FC<ExerciseGuideProps> = ({ exerciseData }) => {
   return (
-    <div>
+    <div className="w-screen">
       <h1 className="w-full flex justify-center">Exercise Guide</h1>
-      <div className="w-10/12 flex justify-center">
+      <div className="w-full h-full flex justify-center">
         {/* exerciseData를 이용한 내용 추가 */}
         {exerciseData.map((exercise) => (
-          <div key={exercise.index} className="w-1/2 border">
+          <div key={exercise.index} className="border w-1/3">
             <p>{exercise.name}</p>
             <Image
               src={`/${exercise.imgurl}.png`}
               alt="homepageCardImage"
               width={250}
               height={250}
+              priority
             />
             <p>Description: {exercise.description}</p>
           </div>
