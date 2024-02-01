5637190d8c333142071312f7158dca16337a4eb5
HongMoonGI <hmoongi94@gmail.com>
2024-02-01 16:02:56 +0900

    feat: 타이머 input값이 바뀔 때 카운트다운 초가 안바뀌는 현상 수정

--- project-exerciseApp/frontend/app/ui/mainpage/timer/timer.tsx
+++ project-exerciseApp/frontend/app/ui/mainpage/timer/timer.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const Timer = () => {
   const [countdown, setCountdown] = useState(0);
@@ -20,11 +20,8 @@ const Timer = () => {
   }, [isActive, countdown]);
 
   useEffect(() => {
-    // 카운트다운이 0이 되면
     if (countdown === 0) {
-      // isActive를 false로 설정하여 pause 버튼이 start로 바뀌게 함
       setIsActive(false);
-      // 초기 세팅 값으로 리셋
       setCountdown(initialCountdown);
     }
   }, [countdown, initialCountdown]);
@@ -39,20 +36,26 @@ const Timer = () => {
   };
 
   const handleSetCountdown = () => {
-    setIsActive(false); // set 버튼 클릭 시 pause로 변경
+    setIsActive(false);
     setCountdown(initialCountdown);
   };
 
   return (
     <div className="w-full h-full flex flex-col justify-center items-center">
-      <h1 className="text-2xl font-bold mb-4">BreakTime: {countdown} seconds</h1>
+      <h1 className="text-2xl font-bold mb-4">
+        BreakTime: {countdown} seconds
+      </h1>
       <label className="flex items-center mb-4">
         Set Countdown:
         <input
           className="ml-2 p-2 border border-gray-300 rounded text-slate-950"
           type="number"
-          defaultValue={countdown}
-          onChange={(e) => setInitialCountdown(Number(e.target.value))}
+          value={initialCountdown} // Use value instead of defaultValue
+          onChange={(e) => {
+            const newValue = Number(e.target.value);
+            setInitialCountdown(newValue);
+            setCountdown(newValue); // Update countdown when input changes
+          }}
         />
       </label>
       <div className="flex space-x-4">
@@ -64,11 +67,11 @@ const Timer = () => {
         </button>
         <button
           className={`${
-            isActive ? 'bg-blue-500' : 'bg-gray-500'
+            isActive ? "bg-blue-500" : "bg-gray-500"
           } text-white px-4 py-2 rounded`}
           onClick={handleStartStop}
         >
-          {isActive ? 'Pause' : 'Start'}
+          {isActive ? "Pause" : "Start"}
         </button>
         <button
           className="bg-red-500 text-white px-4 py-2 rounded"
