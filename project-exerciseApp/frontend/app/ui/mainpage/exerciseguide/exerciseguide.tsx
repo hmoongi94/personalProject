808ae18bbe6461dae5198794207949b84226df1b
HongMoonGI <hmoongi94@gmail.com>
2024-02-01 11:10:25 +0900

    feat: css조정

--- project-exerciseApp/frontend/app/ui/mainpage/exerciseguide/exerciseguide.tsx
+++ project-exerciseApp/frontend/app/ui/mainpage/exerciseguide/exerciseguide.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import Search from "./search";
 
 interface ExerciseData {
   index: number;
@@ -29,29 +30,32 @@ const ExerciseGuide: React.FC<ExerciseGuideProps> = ({ exerciseData }) => {
 
   return (
     <div className="w-screen h-4/5">
-      <h1 className="w-full flex justify-center">Exercise Guide</h1>
+      <h1 className="w-full flex justify-center text-3xl">Exercise Guide</h1>
 
       {/* Primary Category Navigation */}
-      <div className="flex justify-center my-4">
-        <button
-          onClick={() => filterExercisesByCategory(null)}
-          className={`mx-2 px-4 py-2 rounded ${
-            selectedCategory === null ? "bg-pink-500" : "bg-pink-300"
-          }`}
-        >
-          All
-        </button>
-        {primaryCategories.map((category, index) => (
+      <div className="flex justify-end">
+        <div className="flex justify-center my-4 mr-48">
           <button
-            key={index}
-            onClick={() => filterExercisesByCategory(category)}
+            onClick={() => filterExercisesByCategory(null)}
             className={`mx-2 px-4 py-2 rounded ${
-              selectedCategory === category ? "bg-pink-500" : "bg-pink-300"
+              selectedCategory === null ? "bg-pink-500" : "bg-pink-300"
             }`}
           >
-            {category}
+            All
           </button>
-        ))}
+          {primaryCategories.map((category, index) => (
+            <button
+              key={index}
+              onClick={() => filterExercisesByCategory(category)}
+              className={`mx-2 px-4 py-2 rounded ${
+                selectedCategory === category ? "bg-pink-500" : "bg-pink-300"
+              }`}
+            >
+              {category}
+            </button>
+          ))}
+        </div>
+        <Search placeholder="Search exercises..." />
       </div>
 
       {/* Exercise Cards */}
@@ -61,7 +65,7 @@ const ExerciseGuide: React.FC<ExerciseGuideProps> = ({ exerciseData }) => {
             (exercise) =>
               selectedCategory === null ||
               exercise.category.includes(selectedCategory)
-              // includes 메서드를 사용해서 등,하체 이런식으로 두개 합쳐져 있는 카테고리들도 다 뽑아낼 수 있다.
+            // includes 메서드를 사용해서 등,하체 이런식으로 두개 합쳐져 있는 카테고리들도 다 뽑아낼 수 있다.
           )
           .map((exercise, index) => (
             <Link href={`/exercisedetail/${exercise.index}`} key={index}>
--- project-exerciseApp/frontend/app/ui/mainpage/exerciseguide/search.tsx
+++ project-exerciseApp/frontend/app/ui/mainpage/exerciseguide/search.tsx
@@ -19,12 +19,12 @@ export default function Search({ placeholder }: { placeholder: string }) {
   }
 
   return (
-    <div className="relative flex flex-1 flex-shrink-0">
+    <div className="relative w-1/4 h-1/2 flex mt-3">
       <label htmlFor="search" className="sr-only">
         Search
       </label>
       <input
-        className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+        className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-slate-500 outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
         onChange={(e) => {
           handleSearch(e.target.value);
