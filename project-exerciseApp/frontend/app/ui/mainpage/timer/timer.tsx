40a848911cfcf320689966328edbc4c6485fd90f
HongMoonGI <hmoongi94@gmail.com>
2024-02-01 16:09:46 +0900

    feat: 타이머 기능에 실행횟수 추가

--- project-exerciseApp/frontend/app/ui/mainpage/timer/timer.tsx
+++ project-exerciseApp/frontend/app/ui/mainpage/timer/timer.tsx
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
 const Timer = () => {
   const [countdown, setCountdown] = useState(0);
   const [initialCountdown, setInitialCountdown] = useState(0);
   const [isActive, setIsActive] = useState(false);
+  const [executionCount, setExecutionCount] = useState(-2); // New state variable
 
   useEffect(() => {
     let intervalId: NodeJS.Timeout;
@@ -23,6 +24,7 @@ const Timer = () => {
     if (countdown === 0) {
       setIsActive(false);
       setCountdown(initialCountdown);
+      setExecutionCount((prevCount) => prevCount + 1); // Increment execution count
     }
   }, [countdown, initialCountdown]);
 
@@ -33,45 +35,34 @@ const Timer = () => {
   const handleReset = () => {
     setCountdown(initialCountdown);
     setIsActive(false);
-  };
-
-  const handleSetCountdown = () => {
-    setIsActive(false);
-    setCountdown(initialCountdown);
+    setExecutionCount(0); // Reset execution count to its initial value
   };
 
   return (
     <div className="w-full h-full flex flex-col justify-center items-center">
-      <h1 className="text-2xl font-bold mb-4">
-        BreakTime: {countdown} seconds
-      </h1>
+      <h1 className="text-2xl font-bold mb-4">BreakTime: {countdown} seconds</h1>
+      <p className="mb-2">Execution Count: {executionCount}</p>
       <label className="flex items-center mb-4">
         Set Countdown:
         <input
           className="ml-2 p-2 border border-gray-300 rounded text-slate-950"
           type="number"
-          value={initialCountdown} // Use value instead of defaultValue
+          value={initialCountdown}
           onChange={(e) => {
             const newValue = Number(e.target.value);
             setInitialCountdown(newValue);
-            setCountdown(newValue); // Update countdown when input changes
+            setCountdown(newValue);
           }}
         />
       </label>
       <div className="flex space-x-4">
         <button
-          className="bg-green-500 text-white px-4 py-2 rounded"
-          onClick={handleSetCountdown}
-        >
-          Set
-        </button>
-        <button
           className={`${
-            isActive ? "bg-blue-500" : "bg-gray-500"
+            isActive ? 'bg-blue-500' : 'bg-green-500'
           } text-white px-4 py-2 rounded`}
           onClick={handleStartStop}
         >
-          {isActive ? "Pause" : "Start"}
+          {isActive ? 'Pause' : 'Start'}
         </button>
         <button
           className="bg-red-500 text-white px-4 py-2 rounded"
