21fcdc1776c2957e1a29c31f97745b9432dec155
HongMoonGI <hmoongi94@gmail.com>
2024-02-02 17:17:48 +0900

    feat: 운동기록 그래프로 변환

--- project-exerciseApp/frontend/app/ui/mainpage/workoutHistory/workoutHistory.tsx
+++ project-exerciseApp/frontend/app/ui/mainpage/workoutHistory/workoutHistory.tsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect, ChangeEvent } from "react";
+import React, { useState, useEffect } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
-
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from "recharts";
 import "./calender.css";
 
 interface WorkoutEntry {
@@ -16,12 +16,10 @@ const WorkoutHistory: React.FC = () => {
   const [workoutData, setWorkoutData] = useState<WorkoutEntry[]>([]);
 
   useEffect(() => {
-    // Fetch workout data for the selected date from the server
     const fetchData = async () => {
       try {
         const formattedDate = selectedDate.toISOString().split("T")[0];
-
-        const token = localStorage.getItem("token"); // Get the token from local storage
+        const token = localStorage.getItem("token");
 
         const response = await fetch(
           `http://localhost:3560/workoutHistory?date=${formattedDate}`,
@@ -46,36 +44,30 @@ const WorkoutHistory: React.FC = () => {
     };
 
     fetchData();
-    // console.log(selectedDate)
   }, [selectedDate]);
-
-  const handleDateChange = (
-    date: Date | Date[] | null,
-    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  ) => {
-    if (Array.isArray(date)) {
-      // Handle date as an array if needed
-      return;
-    }
-     setSelectedDate(date || new Date())
-  };
 
   return (
     <div className="w-full h-full flex flex-col justify-start items-center">
       <h1 className="text-3xl mb-10">Workout History</h1>
       <div>
-        <Calendar onChange={handleDateChange} value={selectedDate} />
+        <Calendar onChange={(date) => setSelectedDate(date as Date)} value={selectedDate} />
       </div>
       <div>
         <h2>Selected Date: {selectedDate.toISOString().split("T")[0]}</h2>
-        <ul>
-          {workoutData.map((entry) => (
-            <li key={entry.id}>
-              Exercise: {entry.name}, totalReps: {entry.totalReps}, totalSets:{" "}
-              {entry.totalSets}
-            </li>
-          ))}
-        </ul>
+        <ResponsiveContainer width="100%" height={300}>
+          <BarChart
+            data={workoutData}
+            margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="totalReps" fill="#8884d8" />
+            <Bar dataKey="totalSets" fill="#82ca9d" />
+          </BarChart>
+        </ResponsiveContainer>
       </div>
     </div>
   );
