12f1c3b8a230128aea67038170c88ba67f40dab9
HongMoonGI <hmoongi94@gmail.com>
2024-02-14 17:36:41 +0900

    feat: 고유한 값인 postId 데이터베이스에 추가하기

--- project-exerciseApp/backend/routes/community/registerfeed.ts
+++ project-exerciseApp/backend/routes/community/registerfeed.ts
@@ -5,6 +5,16 @@ import { tokenChecker } from "../../utils/tokenChecker";
 
 const registerFeed = express();
 
+function hashCode(str: string): number {
+  let hash: number = 0;
+  for (let i = 0; i < str.length; i++) {
+    const chr: number = str.charCodeAt(i);
+    hash = ((hash << 5) - hash) + chr;
+    hash |= 0; // Convert to 32bit integer
+  }
+  return hash;
+}
+
 //* 이미지 업로드 설정
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -22,30 +32,19 @@ registerFeed.post(
   upload.array("images", 5),
   async (req: Request, res: Response) => {
     // console.log(req.body);
-    
+
     let conn;
 
     // * 쿼리문
     const insertImagesQuery = `
-    INSERT INTO post (userIndex, content, date, imgurl)
-    VALUES (?, ?, ?, ?)
+    INSERT INTO post (userIndex, postId, content, date, imgurl)
+    VALUES (?, ?, ?, ?, ?)
   `;
 
     // *유저인덱스 가져오기
     const userIndex = tokenChecker(req, res);
     if (!userIndex) return console.error("토큰이 없습니다.");
     // console.log(userIndex)
-
-    // *고유한 아이디값 postid에 부여해서 같이 올린게시물 식별하기
-    try {
-      conn = await pool.getConnection();
-      const [rows] = await conn.query('SELECT userId FROM user WHERE userIndex = ?', [userIndex]);
-      console.log(rows.userId)
-      
-    } catch (error) {
-      console.error("Error executing query:", error);
-      console.log("유저테이블에 오류");
-    }
 
     // *현재시간
     const postDate = new Date();
@@ -55,28 +54,46 @@ registerFeed.post(
 
     // console.log(typeof(formattedDate));
 
-    //* 텍스트 내용
-    const content = req.body.text;
-
-    //* 업로드된 이미지 정보 가져오기
-    const files = (req as any).files as Express.Multer.File[];
-
-    // 파일 경로를 DB에 저장
-    const imageurls: string[] = [];
-    files.forEach(async (file) => {
-      const imageurl = file.originalname;
-
-      imageurls.push(imageurl);
-    });
-    // console.log(imageurls)
+    // *고유한 아이디값 postid에 부여해서 같이 올린게시물 식별하기
+    let postId: string;
 
     try {
       conn = await pool.getConnection();
+      const [rows] = await conn.query(
+        "SELECT userId FROM user WHERE userIndex = ?",
+        [userIndex]
+      );
+      const generatePostId = (username: string): string => {
+        const currentTime: number = new Date().getTime(); // 현재 시간을 밀리초로 변환
+        const uniqueString: string = username + currentTime.toString(); // 사용자 이름과 현재 시간을 조합하여 문자열 생성
+        const hash: number = hashCode(uniqueString); // 문자열을 해싱하여 고유한 키 생성
+        return hash.toString(); // 해싱된 값을 문자열로 반환
+      };
+      
+      // console.log(rows.userId);
+      postId = generatePostId(rows.userId);
+      // console.log(postId);
+
+      //* 텍스트 내용
+      const content = req.body.text;
+
+      //* 업로드된 이미지 정보 가져오기
+      const files = (req as any).files as Express.Multer.File[];
+
+      // 파일 경로를 DB에 저장
+      const imageurls: string[] = [];
+      files.forEach(async (file) => {
+        const imageurl = file.originalname;
+
+        imageurls.push(imageurl);
+      });
+      // console.log(imageurls)
 
       if (imageurls.length !== 0) {
         for (const imageurl of imageurls) {
           await conn.query(insertImagesQuery, [
             userIndex,
+            postId,
             content,
             formattedDate,
             imageurl,
@@ -85,6 +102,7 @@ registerFeed.post(
       } else {
         await conn.query(insertImagesQuery, [
           userIndex,
+          postId,
           content,
           formattedDate,
           null, // Set imgurl to null when no images are uploaded
