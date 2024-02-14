1b1da8929e57ab0aba78ab3714e50d970df1a7d3
HongMoonGI <hmoongi94@gmail.com>
2024-02-14 11:52:20 +0900

    feat: console.log(req.files)로 배열이 옴

--- project-exerciseApp/backend/routes/community/registerfeed.ts
+++ project-exerciseApp/backend/routes/community/registerfeed.ts
@@ -7,7 +7,7 @@ const registerFeed = express();
 //* 이미지 업로드 설정
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "frontend/public/community");
+    cb(null, "../frontend/public/community");
   },
   filename: function (req, file, cb) {
     cb(null, file.fieldname + "-" + Date.now() + '.png');
@@ -17,18 +17,18 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 registerFeed.post("/community/registerFeed", upload.array("images", 5), async (req: Request, res: Response) => {
-  console.log(req);
+
+  console.log(req.body)
+  console.log(req.files)
 
   let conn;
   try {
     conn = await pool.getConnection();
-
-
-
+    res.status(200)
     // 응답
   } catch (error) {
-    console.error("Error uploading images:", error);
-    res.status(500).json({ success: false, error: "Error uploading images" });
+    console.error("Error uploading feed:", error);
+    res.status(500).json({ success: false, error: "Error uploading feed" });
   } finally {
     if (conn) conn.release();
   }
