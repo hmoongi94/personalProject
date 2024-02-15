9024f8db0ba610633b1f6d53c9e7ac6d02767bda
HongMoonGI <hmoongi94@gmail.com>
2024-02-15 17:02:56 +0900

    feat: postData 인터페이스 설정

--- project-exerciseApp/frontend/app/(pages)/(main)/(header)/community/editpost/[postId]/page.tsx
+++ project-exerciseApp/frontend/app/(pages)/(main)/(header)/community/editpost/[postId]/page.tsx
@@ -2,16 +2,23 @@
 
 import React, { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
-import EditPost from "@/app/ui/community/editpost/editpost"
+import EditPost from "@/app/ui/community/editpost/editpost";
+
+interface PostData {
+  content: string;
+  imgurl: string;
+}
 
 const EditPostPage = () => {
   const { postId } = useParams(); // postId 가져오기
-  const [postData, setPostData] = useState(null);
+  const [postData, setPostData] = useState<PostData | null>(null);
 
   useEffect(() => {
     const fetchPostData = async () => {
       try {
-        const response = await fetch(`http://localhost:3560/community/editpost/${postId}`);
+        const response = await fetch(
+          `http://localhost:3560/community/editpost/${postId}`
+        );
         const data = await response.json();
 
         if (!response.ok) {
@@ -22,7 +29,10 @@ const EditPostPage = () => {
 
         setPostData(data);
       } catch (error) {
-        console.error("게시물 데이터를 불러오는 동안 에러가 발생했습니다:", error);
+        console.error(
+          "게시물 데이터를 불러오는 동안 에러가 발생했습니다:",
+          error
+        );
       }
     };
 
@@ -33,7 +43,12 @@ const EditPostPage = () => {
     return <div>Loading...</div>;
   }
 
-  return <EditPost initialContent={postData.content} initialImages={postData.imgurl}/>
+  return (
+    <EditPost
+      initialContent={postData.content}
+      initialImages={postData.imgurl}
+    />
+  );
 
   // 수정 폼을 이용하여 postData를 사용하여 게시물을 수정하는 UI를 구현합니다.
 };
