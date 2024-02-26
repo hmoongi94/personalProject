8bd548553061d37d6e80f3e9e772faf93d289b4d
HongMoonGI <hmoongi94@gmail.com>
2024-02-26 14:36:14 +0900

    feat: 이미 존재하는 아이디 확인, 올바른 이메일형식인지 확인하는 로직 추가

--- project-exerciseApp/backend/routes/Signup.ts
+++ project-exerciseApp/backend/routes/Signup.ts
@@ -8,7 +8,7 @@ customerSignup.post("/signup", async (req, res) => {
   let conn;
   const { userId, userPassword, userPassword1, userEmail, userPhoneNumber } =
     req.body;
-  console.log(userPhoneNumber);
+  // console.log(userPhoneNumber);
 
   // userPassword와 userPassword1이 다를 경우 예외 처리
   if (userPassword !== userPassword1) {
@@ -23,10 +23,32 @@ customerSignup.post("/signup", async (req, res) => {
       .json({ success: false, message: "전화번호를 제대로 입력해주세요." });
   }
 
+    // 이메일 형식 검사
+    if (!isValidEmail(userEmail)) {
+      return res
+        .status(253)
+        .json({ success: false, message: "올바른 이메일 형식을 입력해주세요." });
+    }  
+
   try {
     conn = await pool.getConnection();
+
+     // 아이디가 이미 존재하는지 확인하는 쿼리 실행
+     const [existingUser] = await conn.query(
+      "SELECT * FROM user WHERE userId = ?",
+      [userId]
+    );
+    // console.log([existingUser])
+
+    // 이미 존재하는 아이디인 경우 회원가입 막기
+    if ([existingUser].length > 0) {
+      return res
+        .status(252)
+        .json({ success: false, message: "이미 존재하는 아이디입니다." });
+    }
+
     await conn.query(
-      "INSERT INTO user (id, password, email, phonenumber) VALUES (?, ?, ?, ?)",
+      "INSERT INTO user (userId, userPassword, email, phonenumber) VALUES (?, ?, ?, ?)",
       [userId, userPassword, userEmail, userPhoneNumber]
     );
 
@@ -44,3 +66,10 @@ customerSignup.post("/signup", async (req, res) => {
 });
 
 export default customerSignup;
+
+// 이메일 형식 검사 함수
+function isValidEmail(email: string) {
+  // 간단한 이메일 형식 검사 로직
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return emailRegex.test(email);
+}
