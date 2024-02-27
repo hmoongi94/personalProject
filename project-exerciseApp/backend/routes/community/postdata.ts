202b2acec6f915e38fb950f8e6a96102c2673dcc
HongMoonGI <hmoongi94@gmail.com>
2024-02-27 15:55:35 +0900

    fix: (원인) 한 테이블에 두개의 테이블을 join해서 데이터를 추출하다보니 중복되는 값들이 생김. 따로 분리해서 사용함

--- project-exerciseApp/backend/routes/community/postdata.ts
+++ project-exerciseApp/backend/routes/community/postdata.ts
@@ -8,15 +8,24 @@ postData.get("/community/postData", async (req, res) => {
   try {
     conn = await pool.getConnection();
     const result = await conn.query(
-      "SELECT  p.date, p.postId, p.content, p.imgurl, p.userIndex, u.userId, COUNT(l.postIndex) AS likeCount, GROUP_CONCAT(c.commentcontent) AS commentContents, GROUP_CONCAT(c.commentdate) AS commentDates, GROUP_CONCAT(c.userId) AS commentuserId, GROUP_CONCAT(c.commentIndex) AS commentIndexes FROM post p JOIN  user u ON p.userIndex = u.userIndex LEFT JOIN `like` l ON p.postIndex = l.postIndex LEFT JOIN comment c ON p.postIndex = c.postIndex GROUP BY p.postId ORDER BY p.postIndex;"
+      "SELECT p.date, p.postId, p.content, p.imgurl, p.userIndex, u.userId, " +
+        "(SELECT COUNT(*) FROM `like` l WHERE p.postIndex = l.postIndex) AS likeCount, " +
+        "(SELECT GROUP_CONCAT(c.commentcontent) FROM comment c WHERE p.postIndex = c.postIndex) AS commentContents, " +
+        "(SELECT GROUP_CONCAT(c.commentdate) FROM comment c WHERE p.postIndex = c.postIndex) AS commentDates, " +
+        "(SELECT GROUP_CONCAT(c.userId) FROM comment c WHERE p.postIndex = c.postIndex) AS commentuserId, " +
+        "(SELECT GROUP_CONCAT(c.commentIndex) FROM comment c WHERE p.postIndex = c.postIndex) AS commentIndexes " +
+        "FROM post p " +
+        "JOIN user u ON p.userIndex = u.userIndex " +
+        "GROUP BY p.postId, p.date, p.content, p.imgurl, p.userIndex, u.userId " +
+        "ORDER BY p.postIndex;"
     );
     // console.log(result)
-    
+
     res.status(200).json(result);
   } catch (error) {
     console.error("Error fetching products:", error);
     res.status(500).json({ error: "Error fetching products" });
-  } finally {  
+  } finally {
     if (conn) conn.release();
   }
 });
