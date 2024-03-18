08696f9109098ccb27ed8372a6639a293f91e626
HongMoonGI <hmoongi94@gmail.com>
2024-03-18 18:07:54 +0900

    install: dotenv .env파일에 환경변수저장하기

--- project-exerciseApp/backend/database.ts
+++ project-exerciseApp/backend/database.ts
@@ -1,12 +1,15 @@
 import mariadb from "mariadb";
+import dotenv from "dotenv"
+
+dotenv.config();
 
 // MariaDB 연결 풀 설정
 const pool = mariadb.createPool({
   // 집에서 하기 위해 잠깐 바꿧습니다.
-  host: "localhost",
+  host: process.env.db_host,
   port: 3306,
   user: "root",
-  password: "mariadb",
+  password: process.env.db_password,
   database: "personalproject_1",
   connectionLimit: 5,
   supportBigNumbers: true,
